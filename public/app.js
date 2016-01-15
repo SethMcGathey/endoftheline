d726b487ff34a04af9b878861c383f965434db33
MattCordier <mcordier24@gmail.com>
2016-01-14 23:55:49 -0500

    minor update to syntax

--- public/app.js
+++ public/app.js
@@ -517,7 +517,7 @@ jQuery(function($){
                 $('#board').append(App.Host.board[7] + ' <br>');
 		//console.log("Current round" + App.currentRound + " ");
 		$('#board').append(' <br>');
-		App.Host.movePlayerRecursive() //Added by seth
+		App.Host.movePlayerRecursive(); //Added by seth
 		App.Host.squareMaker(5,3,App.Host.square[1] ); 
 	   },		
 	
